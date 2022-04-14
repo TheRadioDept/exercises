8a7f87c6a7a15c8c92c16462a3836dc7b8df6243
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-14 22:42:43 +0400

    Update main.py

--- 1/main.py
+++ 1/main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     count_lines(True)
     count_words(True)
     count_characters(True)
-    count_lines()
+    count_lines(False)
     count_words(False)
     count_characters(False)
