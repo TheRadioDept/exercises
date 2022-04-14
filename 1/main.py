4e0cab53806a5ed9a4445c8e43fd850bc3c409b0
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-14 15:07:52 +0400

    Update main.py

--- 1/main.py
+++ 1/main.py
@@ -2,5 +2,8 @@ from exercise import *
 
 if __name__ == "__main__":
     count_lines(True)
-    count_words()
+    count_words(True)
     count_characters(True)
+    count_lines(False)
+    count_words(False)
+    count_characters(False)
