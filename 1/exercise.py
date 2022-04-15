3aafff6c38eb910c83fe052fddcb0398f6737be1
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-15 10:53:51 +0400

    Update exercise.py

--- 1/exercise.py
+++ 1/exercise.py
@@ -2,51 +2,46 @@ def count_words(ignore_whitespace=False):
     """
     Count number of words in "poem.txt"
     """
-    file = open("poem.txt", "r")
-    data = file.read()
-    words = data.split()
-    if ignore_whitespace:
-        print("Number of words (non empty): {0}".format(len(words)))
-    else:
-        print("Number of words : {0}".format(len(words)))
-
-    file.close()
+    with open("poem.txt") as file:
+        data = file.read()
+        words = data.split()
+        if ignore_whitespace:
+            print("Number of words (non empty): {0}".format(len(words)))
+        else:
+            print("Number of words : {0}".format(len(words)))
 
 
 def count_lines(ignore_whitespace=False):
     """
     Count numbers of Non-empty lines in "poem.txt"
     """
-    file = open("poem.txt", "r")
-    line_count = 0
-    for line in file:
-        if ignore_whitespace:
-            if line != "\n":
+    with open("poem.txt") as file:
+        line_count = 0
+        for line in file:
+            if ignore_whitespace:
+                if line != "\n":
+                    line_count += 1
+            else:
                 line_count += 1
+        if not ignore_whitespace:
+            print("Number of lines : {0}".format(line_count))
         else:
-            line_count += 1
-    if not ignore_whitespace:
-        print("Number of lines : {0}".format(line_count))
-    else:
-        print("Number of lines (non empty): {0}".format(line_count))
-    file.close()
+            print("Number of lines (non empty): {0}".format(line_count))
 
 
 def count_characters(ignore_whitespace=False):
     """
     Count number of characters in "poem.txt"
     """
-    file = open("poem.txt", "r")
-    number_of_characters = 0
-    for line in file:
+    with open("poem.txt") as file:
+        number_of_characters = 0
+        for line in file:
+            if ignore_whitespace:
+                line = line.strip("\n")
+                number_of_characters += len(line)
+            else:
+                number_of_characters += len(line)
         if ignore_whitespace:
-            line = line.strip("\n")
-            number_of_characters += len(line)
+            print("Number of chars (non empty): {0}".format(number_of_characters))
         else:
-            number_of_characters += len(line)
-    if ignore_whitespace:
-        print("Number of chars (non empty): {0}".format(number_of_characters))
-    else:
-        print("Number of chars : {0}".format(number_of_characters))
-
-    file.close()
+            print("Number of chars : {0}".format(number_of_characters))
