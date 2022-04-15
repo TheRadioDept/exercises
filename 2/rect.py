925dd3f520c73a14a428349951365b7388707dcf
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-15 14:49:30 +0400

    Update rect.py

--- 2/rect.py
+++ 2/rect.py
@@ -7,9 +7,7 @@ class Rectangle:
         self.width = width
         self.number = number
 
-    def calculate_area(self, length, width):
-        self.length = length
-        self.width = width
+    def calculate_area(self):
         area = self.length * self.width
         return area
 
@@ -23,7 +21,7 @@ def calculation():
         rectangles[i] = Rectangle(random.randint(1, 100), random.randint(1, 100), i)
     for element in rectangles:
         print(element.length, element.width, sep=" ")
-        areas.append(rectangles[i].calculate_area(random.randint(1, 100), random.randint(1, 100)))
+        areas.append(rectangles[i].calculate_area())
     print("Areas of rectangles are : {} ".format(areas))
     max_area = max(areas)
     print("The max area is {}".format(max_area))
