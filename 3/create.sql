35f91fef4b723f93faecae8a02296fdb2a1c4684
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-21 14:00:42 +0400

    Update create.sql

--- 3/create.sql
+++ 3/create.sql
@@ -3,6 +3,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 drop table if exists usage; 
 drop table if exists device;
 drop table if exists employee; 
+drop type if exists brand;
+drop type if exists dev_type;
+drop type if exists use_type; 
 
 create table employee ( 
     id uuid default uuid_generate_v4 () primary key, 
@@ -12,8 +15,8 @@ create table employee (
     code varchar unique not null
 );
 
-create type brand as enum ('DELL', 'HP', 'SAMSUNG');
-create type dev_type as enum ('Computer', 'Phone', 'Printer');
+create type  brand as enum  ('DELL', 'HP', 'SAMSUNG'); 
+create type  dev_type as enum ('Computer', 'Phone', 'Printer');
 create table device ( 
     id uuid default uuid_generate_v4 () primary key, 
     description varchar not null, 
@@ -30,4 +33,3 @@ create table usage (
     device_id uuid references device (id), 
     type use_type not null
 );
-
