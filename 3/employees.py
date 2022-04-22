45214f56097e6104c4682d2776e2f127390851c2
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-22 12:44:46 +0400

    Update employees.py

--- 3/employees.py
+++ 3/employees.py
@@ -18,15 +18,15 @@ def add_employee():
     """
     conn = psycopg2.connect("dbname = devices user = postgres password = password1")
     cur = conn.cursor()
-    emp_code = input("Enter the code of the employee:")
-    cur.execute(f"SELECT * from EMPLOYEE WHERE code='{emp_code}'")
+    emp_code = input("Enter the code of the employee: ")
+    cur.execute(f"SELECT code from EMPLOYEE WHERE code='{emp_code}'")
     if cur.fetchall():
         print("Code is already taken!")
     elif emp_code == "":
         print("Code cannot be empty")
     else:
-        fname = input("Enter the first name ")
-        lname = input("Enter the last name ")
+        fname = input("Enter the first name: ")
+        lname = input("Enter the last name: ")
         emp_email = input("Enter the email: ")
         if (
             fname != ""
@@ -40,27 +40,31 @@ def add_employee():
             )
             print("Added employee")
         else:
-            print("Invalid parameters")
+            if fname == "":
+                print("Name is empty")
+            elif lname == "":
+                print("Last name is empty")
+            elif emp_email == "":
+                print("Email is empty")
+            elif emp_email.find("@") != 1:
+                print("Not valid email")
     conn.commit()
+    cur.close()
+    conn.close()
 
 
 def list_employee():
     """
     Function to list all employees.
     """
-    try:
-        conn = psycopg2.connect("dbname=devices user = postgres password=password1")
-        cur = conn.cursor()
-        cur.execute(f"SELECT code from EMPLOYEE;")
-        if not cur.fetchall():
-            print("Table is empty")
-        else:
-            cur.execute("SELECT id, first_name, last_name, email, code FROM employee;")
-            record = cur.fetchall()
-            for row in record:
-                print(("{:<14}" * len(row)).format(*row))
-    except NameError:
-        print("An error occured")
+    conn = psycopg2.connect("dbname=devices user = postgres password=password1")
+    cur = conn.cursor()
+    cur.execute("SELECT id, first_name, last_name, email, code FROM employee;")
+    record = cur.fetchall()
+    for row in record:
+        print(("{} " * len(row)).format(*row))
+    cur.close()
+    conn.close()
 
 
 def update_employee():
@@ -71,13 +75,16 @@ def update_employee():
     conn = psycopg2.connect("dbname=devices user = postgres password=password1")
     cur = conn.cursor()
     # print("Enter the updated employee information")
-    code = input("Enter code of the employee ")
+    code = input("Enter code of the employee: ")
+    cur.execute(f"SELECT code from EMPLOYEE WHERE code='{code}'")
     if code == "":
-        print("Code cannot be empty")
+        print("Code cannot be empty ")
+    if not cur.fetchall():
+        print("Code does not exist ")
     else:
-        new_name = input("Enter the first name ")
-        new_lname = input("Enter the last name ")
-        new_email = input("Enter new email ")
+        new_name = input("Enter the first name: ")
+        new_lname = input("Enter the last name: ")
+        new_email = input("Enter new email: ")
         if (
             new_email != ""
             and new_name != ""
@@ -89,8 +96,17 @@ def update_employee():
                 (new_name, new_lname, new_email, code),
             )
         else:
-            print("Invalid parameters")
+            if new_name == "":
+                print("Name is empty ")
+            elif new_lname == "":
+                print("Last name is empty ")
+            elif new_email == "":
+                print("Email is empty ")
+            elif new_email.find("@") != 1:
+                print("Not valid email ")
         conn.commit()
+    cur.close()
+    conn.close()
 
 
 def delete_employee():
@@ -105,31 +121,32 @@ def delete_employee():
     if code == "":
         print("Code cannot be empty")
         sys.exit(1)
-    cur.execute(f"SELECT * from EMPLOYEE WHERE code='{code}'")
+    cur.execute(f"SELECT code from EMPLOYEE WHERE code='{code}'")
     if not cur.fetchall():
         print("No such code!")
     else:
         confirm_delete = input(
-            "Are you sure you want to delete the user with code : {}? ".format(code)
+            "Are you sure you want to delete the user with code : {}? Please type 'yes' to confirm. ".format(
+                code
+            )
         )
         if confirm_delete.lower() == "yes":
             cur.execute("DELETE FROM employee WHERE CODE = %(str)s;", {"str": code})
-            print("User deleted.")
+            print("User deleted. ")
         else:
-            print("Deleting canceled")
+            print("Deleting canceled ")
     conn.commit()
+    cur.close()
+    conn.close()
 
 
-try:
-    if sys.argv[1] == "add_employee":
-        add_employee()
-    elif sys.argv[1] == "list_employee":
-        list_employee()
-    elif sys.argv[1] == "update_employee":
-        update_employee()
-    elif sys.argv[1] == "delete_employee":
-        delete_employee()
-    else:
-        print("No such function")
-except:
-    print("Error occured")
+if sys.argv[1] == "add_employee":
+    add_employee()
+elif sys.argv[1] == "list_employee":
+    list_employee()
+elif sys.argv[1] == "update_employee":
+    update_employee()
+elif sys.argv[1] == "delete_employee":
+    delete_employee()
+else:
+    print("No such function")
