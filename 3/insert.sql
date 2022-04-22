913b984f9376dd99995fa2e29521f8b1b6113316
Farkhad <58568149+TheRadioDept@users.noreply.github.com>
2022-04-22 12:45:34 +0400

    Add files via upload

---
+++ 3/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO 
+    DEVICE(description, brand, type, code) 
+VALUES
+    ('XP13 Laptop', 'DELL', 'Computer', 001),
+    ('Meeting room printer', 'HP', 'Printer', 002),
+    ('Reception printer', 'HP', 'Printer', 003),
+    ('QA phone 1', 'SAMSUNG', 'Phone', 004),
+    ('QA phone 2', 'SAMSUNG', 'Phone', 005);
